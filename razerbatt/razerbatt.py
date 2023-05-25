14760aff55e15dd21f7c595aa261fa02e2fe195e
idonotknowwhoiam <31119114+idonotknowwhoiam@users.noreply.github.com>
2023-05-26 00:15:30 +0300

    Create razerbatt.py

---
+++ razerbatt/razerbatt.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+from openrazer.client import DeviceManager
+
+desired_device = "YOUR DEVICE NAME"
+
+device_manager = DeviceManager()
+
+for device in device_manager.devices:
+	if desired_device in device.name:
+	   desired_device = device
+	else:
+	   print('not found')
+	   exit
+
+is_charging = ''
+if desired_device.is_charging:
+    is_charging = '󱐋'
+
+print(f'{is_charging}{desired_device.battery_level}')
